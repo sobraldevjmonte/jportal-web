e8c836549c66683002ac63b73b94c812fabcdc21
ednaldosobraldev <104601650+ednaldosobraldev@users.noreply.github.com>
2025-01-28 17:59:31 -0300

    ajuste reciredt

--- src/utils/redirec.ts
+++ src/utils/redirec.ts
@@ -3,10 +3,10 @@ export const getBaseUrl = () => {
     console.log("hostname >>>>>>>>>>> " + host);
     if (host === "179.51.199.78:10991") {
       // Ambiente externo
-      return process.env.REACT_APP_LOCAL_BASE_URL;
+      return process.env.REACT_APP_EXTERNAL_BASE_URL;
     } else if (host === "10.5.59.107:3021") {
       // Ambiente interno
-      return process.env.REACT_APP_EXTERNAL_BASE_URL;
+      return process.env.REACT_APP_LOCAL_BASE_URL;
     } else {
       // Ambiente desconhecido ou desenvolvimento
       return process.env.REACT_APP_DEV_BASE_URL;
