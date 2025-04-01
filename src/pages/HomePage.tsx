1b4427cf9a9dff0c5cbbc84ef216ab4db003107a
ednaldosobraldev <104601650+ednaldosobraldev@users.noreply.github.com>
2025-04-01 16:50:27 -0300

    redirect dashboard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef } from "react";
 import MenuComponent from "../common/MenuComponent";
 import LoginPage2 from "./LoginPage2";
 
@@ -8,15 +8,14 @@ import { useNavigate } from "react-router-dom";
 export default function HomePage() {
     const { logado } = useContext(UsuarioContext);
     const navigate = useNavigate();
-    const [redirected, setRedirected] = useState(false); // Novo estado para rastrear o redirecionamento
+    const jaRedirecionou = useRef(false); // Referência para evitar múltiplos redirecionamentos
 
     useEffect(() => {
-        if (logado) {
+        if (logado && !jaRedirecionou.current) {
+            jaRedirecionou.current = true; // Marca que o redirecionamento já ocorreu
             navigate("/dashboard");
-            setRedirected(true); // Marca que o redirecionamento foi realizado
         }
-        
-    }, [logado, redirected, navigate]);
+    }, [logado, navigate]);
 
     return (
         <>
